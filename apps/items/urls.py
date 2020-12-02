8918ea46db76e9b3175a93113ad40f2e778a2129
takisato <tozanarukando11874@ezweb.ne.jp>
2020-12-02 13:16:51 +0900

    add function item_master_delete

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -6,4 +6,5 @@ app_name = "items"
 urlpatterns = [
     path('item_master/', views.item_master, name="item_master"),
     path('item_master/new/', views.item_master_new, name="item_master_new"),
+    path('item_master/<int:item_id>/delete/', views.item_master_delete, name="item_master_delete"),
 ]
--- apps/items/views.py
+++ apps/items/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 from .models import Item
 from .forms import ItemForm
 
@@ -8,14 +9,22 @@ def item_master(request):
     return render(request, 'items/item_master.html', {'items': items})
 
 
+
 def item_master_new(request):
     if request.method == "POST":
         form = ItemForm(request.POST)
         if form.is_valid():
             item = form.save()
-            
+
         return redirect('items:item_master')
 
     else:
         form = ItemForm()
     return render(request, 'items/item_master_new.html', {'form': form})
+
+
+@require_POST
+def item_master_delete(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('items:item_master')
